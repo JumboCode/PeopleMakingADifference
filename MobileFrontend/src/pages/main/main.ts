9014bfb47b438f13faa39ec9637e504e80daf455
Meet Patel <meet.patel@tufts.edu>
2017-10-18 20:09:17 -0400

    added refresh button for users

--- MobileFrontend/src/pages/main/main.ts
+++ MobileFrontend/src/pages/main/main.ts
@@ -7,24 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class MainPage {
 
+  personId: number = 2; //set to specific id temporarily
   personName: string;
   personAssignment: string;
 
   constructor(public navCtrl: NavController) {
+    this.getManifest()
+  }
+
+  onRefreshClick() {
+    this.getManifest()
+  }
+
+  getManifest() {
     // make the HTTPRequest
     // see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-    fetch("http://pmd-server.herokuapp.com")
+    fetch("http://pmd-server.herokuapp.com/"+String(this.personId))
 
     // convert the blob request and JSON parse it asynchronously
     .then((blob) => blob.json())
 
     .then((json) => {
       // pick an index randomly between 0 and the array length - 1
-  		let selectedRandom = Math.floor(Math.random() * json.length);
+      let selectedRandom = Math.floor(Math.random() * json.length);
 
       // set the values that are bound in the template
-  		this.personName = json[selectedRandom].name;
-  		this.personAssignment = json[selectedRandom].assignment;
+      this.personName = json[selectedRandom].name;
+      this.personAssignment = json[selectedRandom].assignment;
     });
   }
 
