3a3cca4917a683eb653c02964283104a6219d03b
Joseph Higgins <blitzkriegmop@gmail.com>
2017-10-24 16:05:39 -0400

    switched starting page to ID Page

--- MobileFrontend/src/app/app.module.ts
+++ MobileFrontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { MainPage } from '../pages/main/main';
+import { IDPage } from '../pages/ID Page/IDPage';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -11,7 +12,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    MainPage
+    MainPage,
+    IDPage
   ],
   imports: [
     BrowserModule,
@@ -20,7 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    MainPage
+    MainPage,
+    IDPage
   ],
   providers: [
     StatusBar,
--- MobileFrontend/src/pages/ID Page/IDPage.ts
+++ MobileFrontend/src/pages/ID Page/IDPage.ts
@@ -1,56 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import * as config from '../../../ionic.config.json';
 
 @Component({
-  selector: 'page-main',
-  templateUrl: 'main.html'
+  selector: 'page-idpage',
+  templateUrl: 'IDPage.html'
 })
-export class MainPage implements OnInit {
+export class IDPage implements OnInit {
 
-  personId: number = 2; //set to specific id temporarily
-  personName: string;
-  personAssignment: string;
+  personId: number;
 
   constructor(public navCtrl: NavController) {}
 
-  ngOnInit(): void {
-    this.getManifest();
-  }
+  ngOnInit(): void {}
 
-  onRefreshClick() {
-    this.getManifest()
-  }
-
-  getManifest() {
+  checkID(id: number) {
     // the api we hit that runs remotely - the "real" one
-    let apiEndpoint = "http://pmd-server.herokuapp.com/";
-
-    // check if we're trying to run locally
-    if(config['DEV_MODE'] === true){
-      apiEndpoint = "http://localhost:5000/";
-    }
+    let apiEndpoint = "http://54.85.114.246:5000/";
 
     // make the HTTPRequest
     // see https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
-    fetch(apiEndpoint + String(this.personId))
+    fetch(apiEndpoint + String(id))
 
     // convert the blob request and JSON parse it asynchronously
     .then((blob) => blob.json())
 
     .then((json) => {
-      if(json.length > 0){
-        // set the values that are bound in the template
-        this.personName = json[0].name;
-        this.personAssignment = json[0].assignment;
-      } else {
-        throw new Error(`JSON response from ${apiEndpoint} formatted incorrectly, expecting at least one result.`);
-      }
+      // do something
     })
     // handle HTTP errors
     .catch((err) => {
-      this.personName = "ERROR";
-      this.personAssignment = "ERROR";
       console.error(err);
       console.error("Try turning on CORS or switching DEV_MODE");
     })
