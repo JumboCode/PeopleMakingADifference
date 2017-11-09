c3d2d73fe675991855bb1d7459b2bfe371a78699
yyang08 <yuyangphoebe@gmail.com>
2017-11-09 12:53:11 -0500

    post update_msg refactored

--- Backend/index.js
+++ Backend/index.js
@@ -117,16 +117,28 @@ app.post("/update_assignment", function(req, res){
 
 // The parameter must be name 'message'
 app.post("/update_message", function(req, res){
-        res.header('Access-Control-Allow-Origin', req.headers.origin);
+       	res.header('Access-Control-Allow-Origin', req.headers.origin);
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	
 	mongodb.MongoClient.connect(uri, function(err, db){
 		if (err)
             throw err;
-		db.collection('message').insert({'message':req.body.message});
-		res.send("Successfully created collection and updated message");
-		});
+
+        var msg = req.body.message;
+        if (/[\\/&;<(*)>$=]/.test( msg )) {
+        	res.send('Invalid input!\n');
+        } else {
+        	coll = db.collection('message').find();
+        	if (coll.length > 0){
+        		db.collection('message').update({'message':msg});
+        	} else {
+        		db.collection('message').insert({'message':msg});
+        	}
+			res.send("Successfully created collection and updated message");
+        }
+	});
 });
 
 
