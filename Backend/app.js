02f78b70e42c56bf78d57ea5aa93aac21d180000
Bhushan Suwal <bhushan_suwal@live.com>
2017-11-17 15:16:53 -0500

    added message to backend seed

--- Backend/app.js
+++ Backend/app.js
@@ -1,22 +1,21 @@
 const mongodb = require('mongodb');
-//var uri = process.env.MONGODB_URI;
+// var uri = process.env.MONGODB_URI;
 const uri = 'mongodb://localhost:27017/pmd';
 test = require('assert');
 
-
 const seedData = [
-	{
-		id: 1,
-		name: 'Tony Stark',
-		assignment: 'Build Ironman suit to escape cave',
+    {
+        id: 1,
+        name: 'Tony Stark',
+        assignment: 'Build Ironman suit to escape cave',
                 location: 'Stark Tower'
-	},
-	{
-		id: 2,
-		name: 'Darth Vader',
-		assignment: 'Use the Force (For evil)',
+    },
+    {
+        id: 2,
+        name: 'Darth Vader',
+        assignment: 'Use the Force (For evil)',
                 location: 'The Death Star'
-	},
+    },
     {
         id: 3,
         name: 'Claire Underwood',
@@ -31,23 +30,32 @@ const seedData = [
     }
  ];
 
+ const messageSeedData = [
+     {
+        message: "You guys are the best! Stay wholesome! <3"
+     }]
 
-// console.log(uri);
+console.log(uri);
 
-// mongodb.MongoClient.connect(uri, function (err, db) {
-//   if (err) {
-//     console.log('Unable to connect to the mongoDB server. Error:', err);
-//   } else {
-//     console.log('Connection established to', uri);
+mongodb.MongoClient.connect(uri, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    console.log('Connection established to', uri);
 
-//     // do some work here with the database.
-//     var data = db.collection('volunteers');
-// 	db.collection('volunteers').insertMany(seedData, function(err, r){
-// 		// test.equal(null, err);
-// 		// test.equal(5, r.insertedCount);
-// 	});
+    // do some work here with the database.
+    var data = db.collection('volunteers');
+    db.collection('volunteers').insertMany(seedData, function(err, r){
+        // test.equal(null, err);
+        // test.equal(5, r.insertedCount);
+    });
 
-//     //Close connection
-//     db.close();
-//   }
-// });
+    var messageData = db.collection('message');
+    db.collection('message').insert(messageSeedData, function(err, r){
+        // test.equal(null, err);
+        // test.equal(5, r.insertedCount);
+    });
+    //Close connection
+    db.close();
+  }
+});
