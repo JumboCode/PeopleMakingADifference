a4160bb7eefb7251bdefbecc8c66e92d39f9a4ec
Kate Zheng <KateZheng@Kates-MacBook-Pro-4.local>
2017-11-17 13:36:48 -0500

    console

--- Backend/index.js
+++ Backend/index.js
@@ -30,6 +30,7 @@ app.get("/", function(req, res){
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     mongodb.MongoClient.connect(uri, function(err, db){
 		if (err) throw err;
+		console.log("travey");
 		result = db.collection('volunteers').find().toArray(function(err, items){
 			res.send(items);
 		});
