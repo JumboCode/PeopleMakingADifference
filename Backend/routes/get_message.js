e91f7e5f3954a170f722805da727994d493f5f2a
Joseph Higgins <josephhiggins@isodevelopers.com>
2018-02-13 17:02:39 -0500

    modified message and checkin to use event ids - no more active

--- Backend/routes/get_message.js
+++ Backend/routes/get_message.js
@@ -1,8 +1,12 @@
 module.exports = function(app, dbconn){
-    app.get('/get_message', function(req, res) {
-        // if message exists return message otherwise return error string
+    app.get('/get_message/:uid', function(req, res) {
+        // if message exists for the event that the user is a member of, return the message, otherwise, an error
         dbconn().then((db) => {
-            existenceCheck = db.collection('bowls').find({active: true}).toArray(function(err, items) {
+            existenceCheck = db.collection('bowls').find(
+                {
+                    'volunteers.id': parseInt(req.params.uid)
+                }
+            ).toArray(function(err, items) {
                 if (items.length > 0) {
                     message = items[items.length-1].message;
                     res.send(String(message));
--- Backend/routes/post_checkin.js
+++ Backend/routes/post_checkin.js
@@ -2,26 +2,38 @@ module.exports = function(app, dbconn){
   // The parameter must be name 'checkout'
     app.post('/update_checkin', function(req, res) {
         dbconn().then((db) => {
-            // if document with argument phone exists then update, otherwise return UID not found
-            console.log(req.body);
-            existenceCheck = db.collection('bowls').find({'volunteers.phone': parseInt(req.body.phone)}, {'volunteers.$': 1}).toArray(function(err, items) {
-                if (items.length > 0) {
-                  if (items[0].volunteers[0].checkin == false) {
-                      db.collection('bowls').update({'volunteers.phone': parseInt(req.body.phone)},
-                          {
-                                $set: {
-                                    'volunteers.$.checkin': Date.now(),
-                                },
-                          });
-                          res.send(items[0].volunteers[0]);
-                    } else {
-                      res.send(items[0].volunteers[0]);
+          // if document with argument phone exists then update, otherwise return UID not found
+          existenceCheck = db.collection('bowls').find(
+            {
+              id: req.body.eventId, 
+              'volunteers.phone': parseInt(req.body.phone)
+            }, 
+            {
+              'volunteers.$': 1
+            }
+          ).toArray(function(err, items) {
+              if (items.length > 0) {
+                if (items[0].volunteers[0].checkin == false) {
+                  db.collection('bowls').update(
+                    {
+                      id: req.body.eventId,
+                      'volunteers.phone': parseInt(req.body.phone)
+                    },
+                    {
+                      $set: {
+                        'volunteers.$.checkin': Date.now(),
+                      }
                     }
+                  );
+                  res.send(items[0].volunteers[0]);
                 } else {
-                    res.send('Error: UID Not Found!');
+                  res.send(items[0].volunteers[0]);
                 }
-                db.close();
-            });
+              } else {
+                res.send('Error: UID Not Found!');
+              }
+              db.close();
+          });
             
         });
     });
