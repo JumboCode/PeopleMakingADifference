e284fa028e9a6692b3755b4c0d874f17bae73a21
Joseph Higgins <josephhiggins@isodevelopers.com>
2018-02-15 22:30:31 -0500

    update post token to match new schema

--- Backend/routes/post_token.js
+++ Backend/routes/post_token.js
@@ -3,18 +3,18 @@ module.exports = function(app, dbconn){
     app.post('/update_token', function(req, res) {
         dbconn().then((db) => {
             // if document with argument id exists then update, otherwise return UID not found
-            console.log(req.body);
-            db.collection('volunteers').find({'id': parseInt(req.body.uid)}).toArray(function(err, items) {
+            db.collection('bowls').find({'volunteers.id': parseInt(req.body.uid)}).toArray(function(err, items) {
                 if (items.length > 0) {
-                    db.collection('volunteers').update({id: parseInt(req.body.uid)},
+                    db.collection('bowls').update({'volunteers.id': parseInt(req.body.uid)},
                         {
-                                    $set: {
-                                        'token': req.body.token,
-                                    },
+                            $set: {
+                                'volunteers.$.token': req.body.token,
+                            },
                         }
                     );
                     res.send('Successfully updated token');
                 } else {
+                    res.status(400);
                     res.send('Error: UID Not Found!');
                 }
                 db.close();
