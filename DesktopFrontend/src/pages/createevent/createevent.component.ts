b7735c292d836d5e86a6f48ddfb99b07f18b07f8
Fripplebubby <blitzkriegmop@gmail.com>
2018-03-12 00:34:57 +0000

    create event screen sends POST data with uploaded file and title

--- DesktopFrontend/src/pages/createevent/createevent.component.ts
+++ DesktopFrontend/src/pages/createevent/createevent.component.ts
@@ -7,14 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CreateEventComponent implements OnInit {
+  
+  csvFile: any;
+  nameFieldText: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSubmitClick(name: string) {
+  onSubmitClick() {
+    let input = new FormData();
+    input.append('csvFile', this.csvFile);
+    input.append('eventName', this.nameFieldText);
     
+    fetch(`http://54.202.94.38:5000/update_event`, {
+      method: 'POST',
+      body: input
+    });
+  }
+  
+  onFileChange(event: any) {
+    if(event.target.files.length > 0) {
+      let file = event.target.files[0];
+      this.csvFile = file;
+    }
+  }
+  
+  onTitleChange(newText: string){
+    this.nameFieldText = newText;
   }
 
 }
