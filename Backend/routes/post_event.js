3fbc5cc7ad3a375291f14a13e58889e68bc0c6e6
Joseph Higgins <josephhiggins@isodevelopers.com>
2018-03-15 18:34:29 -0400

    file checking for csvs, and attempt to parse the uploaded csv (not inserting yet)

--- Backend/routes/post_event.js
+++ Backend/routes/post_event.js
@@ -3,12 +3,36 @@ Example code - not finished!
 All it does is confirm that the files are being uploaded.
 */
 module.exports = function(app, dbconn){
-    const multer = require('multer');
-    const upload = multer({ dest: 'uploads/' });
-    const cpUpload = upload.fields([
-        { name: 'csvFile', maxCount: 1 }, 
-        { name: 'eventName', maxCount: 1 }
-    ]);
+  const multer = require('multer');
+  // define a function that only accepts CSV files
+  const fileFilter = (req, file, cb) => {
+    if(file.fieldname == 'csvFile'
+      &&
+      ( 
+        file.mimetype == 'text/csv'
+        ||
+        file.mimetype == 'application/csv'
+      )
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  }
+
+  const storageHandler = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, './uploads')
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${file.fieldname}-${Date.now()}.csv`)
+    }
+  })    
+
+  const upload = multer({ storage: storageHandler, fileFilter: fileFilter });
+  const cpUpload = upload.fields([
+      { name: 'csvFile', maxCount: 1 }
+  ]);
 	app.post('/update_event', cpUpload, function(req, res, next) {
         // dbconn().then((db) => {
         //      db.collection('bowls').find().toArray(function(err, items) {
@@ -17,7 +41,21 @@ module.exports = function(app, dbconn){
         //     });
             
         // });
-        res.send("hello!");
-        console.log(req.files['csvFile']);
+        if('csvFile' in req.files){
+          const csv = require('../csv.js');
+          const filepath = req.files.csvFile[0].path;
+          console.log('got path', filepath);
+          const parser = new csv(filepath);
+          parser.parse().then(parseResult => {
+            const {rows, feedback} = parseResult;
+            for(let feedbackResult of feedback){
+              console.log(feedbackResult);
+            }
+          })
+        } else {
+          console.log('did not');
+          console.log(req.files);
+        }
+        res.send('lol');
     });
 }
