8e63046855ac26d6cce5cb37cce9af54f1a72d8f
John J. Gallagher <john.gallagher@tufts.edu>
2018-05-17 13:36:54 -0400

    added json parsing for firebase private key

--- Backend/messaging/messaging.js
+++ Backend/messaging/messaging.js
@@ -6,7 +6,7 @@ const firebaseCreds = {
   token_uri: "https://accounts.google.com/o/oauth2/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
